68013e0756c1e7f4ca5f1af7b80d3dc1753b76cb
Prashant <prashantamoli@outlook.com>
2023-01-25 19:28:06 +0530

    bg fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import { Navbar, MovieModal, Homepage, Search, MovieSearch, TVSearch, Favourites
 
 export default function App() {
 	return (
-		<>
-			<div className="absolute -z-50 top-0 left-0 bottom-0 bg-gradient-to-l from-gray-500 via-gray-200 to-gray-500 hover:bg-gradient-to-r hover:from-gray-500 hover:via-gray-300  hover:to-gray-400 min-h-screen w-full oveflow-x-hidden">
-			</div>
+		<main className="w-full">
+			<div className="fixed -z-50 p-0 box-border inset-0 bg-gradient-to-l from-gray-500 via-gray-200 to-gray-500 hover:bg-gradient-to-r hover:from-gray-500 hover:via-gray-300  hover:to-gray-400 min-h-screen w-full overflow-x-hidden"></div>
 
 			<Navbar />
 			<MovieModal />
@@ -17,6 +16,6 @@ export default function App() {
 				<Route path="/tv" element={<TVSearch />} />
 				<Route path="/favourites" element={<Favourites />} />
 			</Routes>
-		</>
+		</main>
 	);
 }
